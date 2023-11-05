9161e7da36621ea1b2976b8028a88b963833401b
Kravets <57632712+kravetsone@users.noreply.github.com>
2023-11-05 17:19:23 +0300

    feat(structures): add some other structures

---
+++ src/structures/location.ts
@@ -0,0 +1,58 @@
+import { Inspect, Inspectable } from "inspectable";
+import { TelegramLocation } from "../generated";
+
+/** This object represents a point on the map. */
+@Inspectable()
+export class Location {
+    constructor(public payload: TelegramLocation) {}
+
+    get [Symbol.toStringTag]() {
+        return this.constructor.name;
+    }
+
+    /** Longitude as defined by sender */
+    @Inspect()
+    get longitude() {
+        return this.payload.longitude;
+    }
+
+    /** Latitude as defined by sender */
+    @Inspect()
+    get latitude() {
+        return this.payload.latitude;
+    }
+
+    /** The radius of uncertainty for the location, measured in meters; `0-1500` */
+    @Inspect()
+    get horizontalAccuracy() {
+        return this.payload.horizontal_accuracy;
+    }
+
+    /**
+     * Time relative to the message sending date,
+     * during which the location can be updated, in seconds.
+     * For active live locations only.
+     */
+    @Inspect()
+    get livePeriod() {
+        return this.payload.live_period;
+    }
+
+    /**
+     * The direction in which user is moving, in degrees; `1-360`.
+     * For active live locations only.
+     */
+    @Inspect()
+    get heading() {
+        return this.payload.heading;
+    }
+
+    /**
+     * Maximum distance for proximity alerts about approaching another chat member, in meters.
+     * For sent live locations only.
+     */
+    @Inspect()
+    get proximityAlertRadius() {
+        return this.payload.proximity_alert_radius;
+    }
+}
---
+++ src/structures/story.ts
@@ -0,0 +1,12 @@
+import { TelegramStory } from "../generated";
+
+/**
+ * This object has not been completed yet
+ */
+export class Story {
+    constructor(public payload: TelegramStory) {}
+
+    get [Symbol.toStringTag]() {
+        return this.constructor.name;
+    }
+}
